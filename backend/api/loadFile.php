9b0090111e56eae25b36ef8794a1b1ad2e16b450
haile <haileasaye51@gmail.com>
2025-05-18 14:57:04 +0300

    loadfile or open file is done

--- backend/api/loadFile.php
+++ backend/api/loadFile.php
@@ -1,41 +1,34 @@
 <?php
-require_once '../config/config.php';
+require_once '../config/database';
 require_once '../core/FileManager.php';
 require_once '../core/helpers.php';
+//parse the incoming json
 
 $data =json_decode(file_get_contents('php://input'),true);
 
+//validate input
 
-if(!isset($data['fileName'])){
-    jsonResponse('error','filename is required');
+if(!isset($data['filename'])){
+    jsonResponse('error','File name is required');
 
 }
 
+$filename= sanitizeFileName($data['filename']);
+$fileManager =new FileManager(BASE_PATH, VERSION_PATH);
 
-$fileName = sanitizeFilename($data['fileName']);// to prevent security problems and remove dangerious characters 
+try{
+    $content= $fileManager->load($filename);
 
-$fileManager = new FileManager(BASE_PATH, VERSION_PATH);
-$content =$fileManager->load($fileName);// try to read the contents of the file and if it exits you get the text inside if it does not it returns false 
+    if($content===false){
+        jsonResponse('error', 'File not found or can not be read');
 
+    }
+    jsonResponse('successs', 'File loaded successfully.', ['content'=$content]);
 
-if($content!==false){
-    jsonResponse('success','File Loaded', ['content'=>$content]);
 }
-else {
-    jsonResponse('error', 'File Not found');
+catch(Exception $e){
+    jsonResponse('error', 'An error occured while loading the file'.$e->getMessage());
+
 }
-
-
-
-?>
-
-
-
-
-
-
-
-
-
 
 ?>
